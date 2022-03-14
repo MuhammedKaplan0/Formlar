854d88e40c360d1797365c6d0ab82619ae74e527
MuhammedKaplan0 <100554179+MuhammedKaplan0@users.noreply.github.com>
2022-03-14 10:15:40 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Formlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("enter tuşu çalışıyor");    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("esc tuşu çalışıyor");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.ControlBox == true)
+
+                this.ControlBox = false;
+            else 
+                this.ControlBox = true;
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        this.CenterToScreen();
+        }
+         
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 form2=new Form2();
+            form2.Show();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form2 form2=new Form2();
+            form2.ShowDialog();
+        }
+       
+    }
+    }
+
---
+++ Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Formlar
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void FormB_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        MessageBox.Show("FormB kapanıyor.");
+        }
+         private void FormB_FormClosed(object sender, FormClosedEventArgs e)
+{
+MessageBox.Show("FormB kapandı.");
+}
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+             this.Hide();
+         }
+
+         private void button2_Click(object sender, EventArgs e)
+         {
+             this.Close();
+         }
+         
+        private void FormB_Click(object sender, EventArgs e)
+{
+MessageBox.Show("FormB click olayı çalışıyor.");
+}
+   
+    }
+}
